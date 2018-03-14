bad4761c9be6130cfc6961c90474c9fc054cbaf9
Yura Vusach <Yura_Vusach@epam.com>
2018-03-14 15:34:35 +0200

    adding multi module project

---
+++ root/hospitalAPIs/storage/src/main/java/com/hospital/storage/StorageApplication.java
@@ -0,0 +1,12 @@
+package com.hospital.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageApplication.class, args);
+	}
+}
