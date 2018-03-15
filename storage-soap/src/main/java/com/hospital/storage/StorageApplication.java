e02934ace550d63cd423c622fe8509be1772b9e9
Yura Vusach <Yura_Vusach@epam.com>
2018-03-15 11:13:58 +0200

    new multi module project

--- storage-soap/src/main/java/com/hospital/storage/StorageApplication.java
+++ storage-soap/src/main/java/com/hospital/storage/StorageApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StorageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StorageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApplication.class, args);
+    }
 }
