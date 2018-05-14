734ba90f11c288b37325c9f08c127384688d1978
Yura Vusach <Yura_Vusach@epam.com>
2018-05-14 20:11:37 +0300

    swagger config adding

--- hospital-rest/src/main/java/com/hospital/core/controller/UserController.java
+++ hospital-rest/src/main/java/com/hospital/core/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
