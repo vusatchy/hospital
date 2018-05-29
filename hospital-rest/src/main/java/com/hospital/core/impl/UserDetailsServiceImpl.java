fc10b478ddcdd414134d1f9cc85b9a8e1d9a3182
Yura Vusach <Yura_Vusach@epam.com>
2018-05-29 10:39:59 +0300

    user services merged

--- hospital-rest/src/main/java/com/hospital/core/impl/UserDetailsServiceImpl.java
+++ hospital-rest/src/main/java/com/hospital/core/impl/UserDetailsServiceImpl.java
@@ -10,18 +10,30 @@ import com.hospital.core.exceptions.UserAlreadyExistException;
 import com.hospital.core.model.Role;
 import com.hospital.core.model.User;
 import com.hospital.core.respository.UserRepository;
+import com.hospital.core.security.UserPrincipalImpl;
 import com.hospital.core.service.UserService;
 import com.hospital.core.util.EncryptionUtil;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-
 @Service
-public class UserServiceImp implements UserService {
+public class UserDetailsServiceImpl implements UserDetailsService, UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+	User user = userRepository.findByName(username);
+	if (user == null) {
+	    throw new UsernameNotFoundException(username);
+	}
+	return new UserPrincipalImpl(user);
+    }
 
     @Override
     public User saveUser(String name, String email, String password)
--- hospital-rest/src/main/java/com/hospital/core/security/WebSecurityConfig.java
+++ hospital-rest/src/main/java/com/hospital/core/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hospital.core.security;
 
+import com.hospital.core.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
